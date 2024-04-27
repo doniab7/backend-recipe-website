b0230b6fb00770c734f9a3adcd3d59eef0b247fa
Donia <doniabougrine7@gmail.com>
2024-04-28 00:22:54 +0100

    create meal and find one fix

--- src/meal/meal.module.ts
+++ src/meal/meal.module.ts
@@ -3,9 +3,11 @@ import { MealService } from './meal.service';
 import { MealController } from './meal.controller';
 import { Meal } from '../entities/meal.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ingredient } from 'src/entities/ingredient.entity';
+import { Step } from 'src/entities/step.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Meal])],
+  imports: [TypeOrmModule.forFeature([Meal, Ingredient, Step])],
   controllers: [MealController],
   providers: [MealService],
 })
