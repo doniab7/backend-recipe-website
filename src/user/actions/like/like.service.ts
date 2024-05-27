60714c0f2949905dc8f96e274cedaf9a9f92f85b
oumayma159 <oumaima.elfidha@insat.ucar.tn>
2024-05-27 04:27:05 +0100

    fix like service

--- src/user/actions/like/like.service.ts
+++ src/user/actions/like/like.service.ts
@@ -1,39 +1,79 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '../../../entities/user.entity';
-import { Meal } from '../../../entities/meal.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
+import { User } from '../../../entities/user.entity';
+import { Meal } from 'src/entities/meal.entity';
 
 @Injectable()
 export class LikeService {
   constructor(
-    @InjectRepository(Meal)
-    private mealRepository: Repository<Meal>,
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(Meal)
+    private mealRepository: Repository<Meal>,
   ) {}
 
-  async likeMeal(userId: string, mealId: string): Promise<void> {
-    const meal = await this.mealRepository.findOne({ where: { id: mealId }, relations: ['usersWhoLiked'] });
-    const user = await this.userRepository.findOne({ where: { id: userId } });
+  async likeMeal(id: string, mealId: string) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.likedMeals', 'likedMeals')
+      .where('user.id = :id', { id: id })
+      .getOne();
 
-    if (meal && user) {
-      meal.usersWhoLiked.push(user);
-      meal.numberLikes++;
-      await this.mealRepository.save(meal);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const meal = await this.mealRepository
+      .createQueryBuilder('meal')
+      .leftJoinAndSelect('meal.usersWhoLiked', 'usersWhoLiked')
+      .where('meal.id = :id', { id: mealId })
+      .getOne();
+
+    if (!meal) {
+      throw new NotFoundException('Meal not found');
+    }
+
+    if (user.likedMeals.find((likedMeal) => likedMeal.id === meal.id)) {
+      return { message: 'Meal already liked' };
+    } else {
+      user.likedMeals.push(meal);
+      await this.userRepository.save(user);
+      return { message: 'Meal successfully liked' };
     }
   }
 
-  async unlikeMeal(userId: string, mealId: string): Promise<void> {
-    const meal = await this.mealRepository.findOne({ where: { id: mealId }, relations: ['usersWhoLiked'] });
-    const user = await this.userRepository.findOne({ where: { id: userId } });
+  async getLikes(id: string) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.likedMeals', 'likedMeals')
+      .where('user.id = :id', { id: id })
+      .getOne();
 
-    if (meal && user) {
-      meal.usersWhoLiked = meal.usersWhoLiked.filter(u => u.id !== userId);
-      meal.numberLikes--;
-      await this.mealRepository.save(meal);
+    return user.likedMeals;
+  }
+
+  async unlikeMeal(userId: string, mealId: string) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.likedMeals', 'likedMeals')
+      .where('user.id = :id', { id: userId })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
+    const likeIndex = user.likedMeals.findIndex((meal) => meal.id === mealId);
+
+    if (likeIndex === -1) {
+      throw new NotFoundException('Like not found');
+    }
+
+    user.likedMeals.splice(likeIndex, 1);
+
+    await this.userRepository.save(user);
+
+    return { message: 'Like removed successfully' };
   }
 }
-
