4f1250f2b0aa77f8286a5e91370bd08d4e7ba3e9
Donia Bougrine <124002250+doniab7@users.noreply.github.com>
2024-05-27 05:02:27 +0100

    fix like

--- src/user/actions/like/like.controller.ts
+++ src/user/actions/like/like.controller.ts
@@ -1,19 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { LikeService } from './like.service';
 import { User } from '../../../user/user.decorator';
-
+import { JwtAuthGuard } from 'src/user/Guards/jwt-auth.guard';
 
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post(':mealId/like')
-  async likeMeal(@Param('mealId') mealId: string, @User() user: any) {
+  async likeMeal(@Param('mealId') mealId: string, @User() user) {
     return this.likeService.likeMeal(user.id, mealId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':mealId/unlike')
-  async unlikeMeal(@Param('mealId') mealId: string, @User() user: any) {
+  async unlikeMeal(@Param('mealId') mealId: string, @User() user) {
     return this.likeService.unlikeMeal(user.id, mealId);
   }
 }
